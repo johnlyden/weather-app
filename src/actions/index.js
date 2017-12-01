c2092e16ee1ad0e163bddcff6e09114cdb52ca34
johnlyden <john.lyden86@gmail.com>
2017-11-30 21:01:48 -0600

    made url with the api_key and city name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = '396101bd5e40f5d5c50350de977eec6c';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    };
+}
