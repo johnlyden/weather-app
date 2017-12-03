b75c571e6ba826d4f6ed33c2ab6fd5c753971086
johnlyden <john.lyden86@gmail.com>
2017-12-02 21:05:06 -0600

    updated weather reducer to add new state and return new state array

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,8 @@
-export default function(state = null, action) {
-    console.log('action received', action);
-    return state;
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+    case FETCH_WEATHER:
+        return [action.payload.data, ...state ];
+    }
 }
