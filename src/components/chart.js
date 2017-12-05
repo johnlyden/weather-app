484979922496f8e3e3df35110105305bd72a81f9
johnlyden <john.lyden86@gmail.com>
2017-12-05 12:58:49 -0600

    added charts to a card

--- src/components/chart.js
+++ src/components/chart.js
@@ -12,12 +12,13 @@ function average(data) {
 export default (props) => {
     return (
         <div>
+          <Card>
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color} />
                 <SparklinesReferenceLine type="avg" />
             </Sparklines>
             <div>{average(props.data)} {props.units}</div>
-          <Divider />
+          </Card>
         </div>
    );
 }
