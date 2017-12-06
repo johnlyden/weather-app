8147a0d9a5618bc275adbc3e02fce6d08f969bc8
johnlyden <john.lyden86@gmail.com>
2017-12-05 19:46:56 -0600

    added google maps integration

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -8,13 +9,16 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
-
+        const { lon, lat } = cityData.city.coord;
+        
+        
+        
         return (
-            <tr key={name}>
-                <td> {name} </td>
-                <td><Chart data={temps} color="orange" units="K"/></td>
-                <td><Chart data={pressures} color="green" units="hPa" /></td>
-                <td><Chart data={humidities} color="black" units="%"/></td>
+            <tr key={ name } >
+                <td><GoogleMap lon={ lon } lat={ lat } /></td>
+                <td><Chart data={ temps } color="orange" units="K"/></td>
+                <td><Chart data={ pressures } color="pink" units="hPa"/></td>
+                <td><Chart data={ humidities } color="red" units="%"/></td>                
             </tr>
         );
     }
@@ -27,20 +31,19 @@ class WeatherList extends Component {
                         <th>City</th>
                         <th>Temperature (K)</th>
                         <th>Pressure (hPa)</th>
-                        <th>Humidity (%)</th>
+                        <th>Humidity (%) </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.props.weather.map(this.renderWeather)}
+                    { this.props.weather.map(this.renderWeather) }
                 </tbody>
             </table>
         );
     }
-
 }
 
 function mapStateToProps({ weather }) {
-    return { weather };
+    return { weather }; // { weather } === { weather: weather }
 }
 
 export default connect(mapStateToProps)(WeatherList);
